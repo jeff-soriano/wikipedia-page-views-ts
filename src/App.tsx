930936e7253d5264f31700d5d5ab80ed6a981de8
Jeff Soriano <soriano.jeffm@gmail.com>
2023-08-30 14:31:23 -0700

    Implement state for date and getArticles function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [date, setDate] = useState(new Date().toISOString().substring(0, 10))
+
+  const getArticles = (date: string) => {
+    const year = date.substring(0, 4)
+    const month = date.substring(5, 7)
+    const day = date.substring(8, 10)
+
+    fetch(
+      `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${year}/${month}/${day}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
   return (
     <div>
       <header>
@@ -8,7 +25,13 @@ function App() {
       </header>
       <main>
         <div>
-          <input type="date" id="wikipedia-date" name="wikipedia-date" />
+          <input
+            type="date"
+            id="wikipedia-date"
+            name="wikipedia-date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
           <select name="num-articles" id="num-articles">
             <option value={25}>25</option>
             <option value={50}>50</option>
@@ -16,7 +39,7 @@ function App() {
             <option value={100}>100</option>
             <option value={200}>200</option>
           </select>
-          <button>Search</button>
+          <button onClick={() => getArticles(date)}>Search</button>
         </div>
         <ol>
           <p>1</p>
