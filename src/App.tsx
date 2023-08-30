a11317e14241386c586f8f276caf5d5865a78043
Jeff Soriano <soriano.jeffm@gmail.com>
2023-08-30 13:58:36 -0700

    Add initial skeleton implementation of design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Top Wikipedia Articles</h1>
       </header>
+      <main>
+        <div>
+          <input type="date" id="wikipedia-date" name="wikipedia-date" />
+          <select name="num-articles" id="num-articles">
+            <option value={25}>25</option>
+            <option value={50}>50</option>
+            <option value={75}>75</option>
+            <option value={100}>100</option>
+            <option value={200}>200</option>
+          </select>
+          <button>Search</button>
+        </div>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+        <ol>
+          <p>1</p>
+          <p>The Last of Us (TV Show)</p>
+          <p>5,555,555 views</p>
+        </ol>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
