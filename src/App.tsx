8689e853e867e5e198c8190597bc872931df7890
Jeff Soriano <soriano.jeffm@gmail.com>
2023-08-30 21:44:15 -0700

    Style date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import ReactDatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import dateSvg from './images/date.svg'
+
 var classNames = require('classnames')
 
 type ArticleObj = {
@@ -9,17 +13,21 @@ type ArticleObj = {
 }
 
 function App() {
-  const [date, setDate] = useState(new Date().toISOString().substring(0, 10))
+  const [date, setDate] = useState(new Date())
   const [numResults, setNumResults] = useState(100)
   const [articles, setArticles] = useState<ArticleObj[]>([])
   const [currentPage, setCurrentPage] = useState(0)
+  const [isSelectingDate, setIsSelectingDate] = useState(false)
 
   const numPages = Math.ceil(articles.length / 10)
 
-  const getArticles = (date: string, numResults: number) => {
-    const year = date.substring(0, 4)
-    const month = date.substring(5, 7)
-    const day = date.substring(8, 10)
+  const getArticles = (date: Date, numResults: number) => {
+    const year = date.getFullYear()
+    // I got the following from:
+    // https://stackoverflow.com/questions/6040515/how-do-i-get-month-and-date-of-javascript-in-2-digit-format
+    // This is to get month/day in MM/DD format, which date.get... won't do out of the box
+    const month = ('0' + (date.getMonth() + 1)).slice(-2)
+    const day = ('0' + date.getDate()).slice(-2)
 
     fetch(
       `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${year}/${month}/${day}`
@@ -101,18 +109,45 @@ function App() {
     )
   }
 
+  const DateButton = () => (
+    <button
+      className="flex items-center"
+      onClick={() => setIsSelectingDate(true)}
+    >
+      <img src={dateSvg} alt="Your SVG" />
+      <div className="text-left ml-4 mr-6">
+        <div className="text-xs">DATE ^</div>
+        <div>
+          {date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            timeZone: 'UTC',
+          })}
+        </div>
+      </div>
+    </button>
+  )
+
   return (
     <div className="bg-slate-50">
       <div className="h-20 bg-white"></div>
       <main className="flex flex-col items-center w-7/12 mx-auto py-8">
         <h1 className="text-4xl mb-10">Top Wikipedia Articles</h1>
-        <div className="bg-white mb-5 flex justify-between w-full">
-          <input
-            type="date"
-            id="wikipedia-date"
-            name="wikipedia-date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+        <div className="bg-white mb-5 flex justify-between w-full rounded-full px-4 py-3">
+          <ReactDatePicker
+            selected={date}
+            onChange={(date) => date && setDate(date)}
+            customInput={DateButton()}
+            className={classNames(
+              {
+                'bg-slate-100': isSelectingDate,
+                'hover:bg-slate-50': !isSelectingDate,
+              },
+              'rounded-full px-3 py-2 font-poppins'
+            )}
+            onCalendarClose={() => setIsSelectingDate(false)}
+            onCalendarOpen={() => setIsSelectingDate(true)}
           />
           <select
             name="num-articles"
