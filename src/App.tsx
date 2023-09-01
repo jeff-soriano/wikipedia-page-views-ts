3426c86df54aee752f29e96f11968a4714c42724
Jeff Soriano <soriano.jeffm@gmail.com>
2023-08-31 21:32:31 -0700

    Change width

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
   return (
     <div>
       <div className="h-20 bg-white"></div>
-      <main className="flex flex-col items-center w-6/12 mx-auto py-8">
+      <main className="flex flex-col items-center w-2/3 mx-auto py-8">
         <h1 className="text-4xl mb-10">Top Wikipedia Articles</h1>
         <div className="bg-white mb-5 flex justify-between w-full rounded-full px-4 py-3">
           <DatePicker
